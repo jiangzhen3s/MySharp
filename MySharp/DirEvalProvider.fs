39fcc373b588823f8346d013dfcec4afc5a839b0
jiangzhen <410096427@qq.com>
2015-02-12 22:30:31 +0800

    add comments

--- MySharp/DirEvalProvider.fs
+++ MySharp/DirEvalProvider.fs
@@ -5,54 +5,40 @@ open System.IO
 open System.Text
 open System.Collections.Generic
 
+//properties env
 let propEnv = new Dictionary<string, Dictionary<string, string>>()
-let varEnv : (string * string )list = []
-
-let setVar name value varenv =
-    (name,value)::varenv
-let getVar name varenv =
-    List.tryFind( fun (name',v)->name=name') varenv
-
-//let newVarEnv = setVar "a" "b" varEnv
-//getVar "a" newVarEnv
-
+// var env
+let varEnv : (string * string) list = []
+let setVar name value varenv = (name, value) :: varenv
+let getVar name varenv = List.tryFind (fun (name', v) -> name = name') varenv
+//get directory info
 let getDirInfo path = new DirectoryInfo(path)
+//get sub dirs
 let getSubDirs currentDirPath = Directory.GetDirectories(currentDirPath) |> Array.map getDirInfo
 
+// get dir all public properties
 let getAllValue obj = 
     let t = obj.GetType()
     t.GetProperties() |> Array.map (fun p -> (p.Name.ToLower(), p.GetValue(obj).ToString()))
 
+// find properties values
 let filterValue names nameValues = 
     nameValues |> Array.filter (fun (name, value) -> 
                       match names |> Array.tryFind (fun _name -> _name = name) with
                       | Some(_) -> true
                       | None -> false)
 
+// get dir info properties
 let getValues names obj = getAllValue obj |> filterValue names
 
-//let formatAll (env':Dictionary<string, Dictionary<string, string>>) =
-//    let ksb =new StringBuilder()
-//    let vsb =new StringBuilder()
-//    for dir in (env'.Keys) do
-//        for propertyName in env'.[dir].Keys do
-//            ksb.Append(propertyName+" ")  |> ignore   
-//            vsb.Append(env'.[dir].[propertyName]+" ") |> ignore
-//
-//            
-//let evalexpr expr env =
-//    match expr with
-//    | Star ->
+//eval absyn 
 let eval stmt = 
     match stmt with
     | Select(exprs, dirs, Some(whereExprs)) -> ()
     | Select(exprs, dirs, None) -> ()
     | Set(name, str) -> ()
-
-
-
 (*
-examples:
+stmt examples:
 
 SET c <- 'C:\\Temp'
 SELECT * FROM c --query all properties about directory info 
